c59d401b2197f372a2f85cd42ab2a55d23a3b284
gmarseglia <g.marseglia.it@gmail.com>
2022-11-03 12:21:11 +0100

    switch mode added to wacom.sh

--- Script/wacom.sh
+++ Script/wacom.sh
@@ -1,22 +1,35 @@
 # !/bin/bash
+
+MODE_TO_SET=""
+
 # Configure Wacom Tablet
 WACOM_ID=`xsetwacom --list | grep stylus | awk '{print $9}'`
 xsetwacom --set $WACOM_ID Button 2 "pan"
 xsetwacom --set $WACOM_ID PanScrollThreshold 250
 xsetwacom --set $WACOM_ID CursorProximity 15
 
-if [[ -z $1 ]] || [[ $1 == "-r" ]];
-	then
-		MODE="Relative";
+if [[ -z $1 ]] || ([[ $1 == '-s' ]] || [[ $1 == '--switch' ]]) ; then
+	MODE_GET=$(xsetwacom --get $WACOM_ID Mode)
+	if [[ $MODE_GET == "Relative" ]] ; then
+		MODE_TO_SET="Absolute" ;
 	else
-		if [[ $1 == "-a" ]] ; then MODE="Absolute" ; fi
+		MODE_TO_SET="Relative" ;
+	fi
 fi
 
-if [[ $MODE != "Relative" ]] && [[ $MODE != "Absolute" ]] ; then
+if [[ -z $MODE_TO_SET ]] && ([[ $1 == '-r' ]] || [[ $1 == '--relative' ]]) ; then
+	MODE_TO_SET="Relative";
+fi
+
+if [[ -z $MODE_TO_SET ]] && ([[ $1 == '-a' ]] || [[ $1 == '--absolute' ]]) ; then
+	MODE_TO_SET="Absolute";
+fi
+
+if [[ -z $MODE_TO_SET ]] ; then
 	printf "Incorrect usage.\n"
 	exit 1 ;
 fi
 
-xsetwacom --set $WACOM_ID Mode $MODE
+xsetwacom --set $WACOM_ID Mode $MODE_TO_SET
 
-printf "MODE set to $MODE\n"
+notify-send "Mode of id:$WACOM_ID set to $MODE_TO_SET"
